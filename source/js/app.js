4ea995fbe2f63143111139be5bfd71f606de0561
Alexander <dev@salex.pro>
2018-04-03 13:00:58 +0300

    bower to npm

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,8 @@
 /* 
-@codekit-prepend '../../bower_components/jquery/dist/jquery.min.js';
-@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.core.min.js';
-@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js';
-@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
+@codekit-prepend '../../node_modules/jquery/dist/jquery.min.js';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min.js';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
 */
 
-// $(document).foundation();
+$(document).foundation();
