85dea3fdd189e9c6c3e3a7576e294b08be573dd0
Alexander <dev@salex.pro>
2018-04-12 01:21:37 +0300

    change project structure

--- source/js/app.js
+++
@@ -1,8 +0,0 @@
-/* 
-@codekit-prepend '../../node_modules/jquery/dist/jquery.min.js';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min.js';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
-*/
-
-$(document).foundation();
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+/* 
+@codekit-prepend quiet '../../node_modules/jquery/dist/jquery.min.js',
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min.js';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
+*/
+
+$(document).foundation();
