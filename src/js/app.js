a310f07d2f005baf6e7fe3e7865a0dd0484576ea
Alexander <dev@salex.pro>
2019-04-08 01:59:55 +0300

    add ability to use es6 modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,1 @@
-/* 
-@codekit-prepend quiet '../../node_modules/jquery/dist/jquery.min',
-@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min';
-@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min';
-@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min';
-*/
-
-$(document).foundation();
+import './lib/foundation-explicit-pieces';
