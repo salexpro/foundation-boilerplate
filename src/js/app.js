8bd18dfa67caaea5f1b14938262a160541a703b7
Alexander <dev@salex.pro>
2019-04-24 18:08:03 +0300

    add svg sprites support

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 // import polyfill from '@babel/polyfill';
+
+import svg4everybody from 'svg4everybody';
+// import './lib/foundation-explicit-pieces';
+
+svg4everybody();
